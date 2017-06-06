54af7e61d9cf11d820f86a754710297d223e9944
squeemishly <squeemishly@gmail.com>
2017-06-06 10:47:59 -0600

    user can see all tags associated with a job

--- spec/features/tags/user_sees_the_name_of_a_tag_associated_with_a_job_spec.rb
+++ spec/features/tags/user_sees_the_name_of_a_tag_associated_with_a_job_spec.rb
@@ -1,15 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe "a user can see a tag associated with a job" do
-  scenario "when they visit a specific job page" do
+RSpec.describe "a user can see tags associated with a job when they visit a job page" do
+  scenario "they can see one tag" do
     company = Company.create!(name: "ESPN")
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
     tag = Tag.create(name: "Developer")
     JobTag.create(job_id: job.id, tag_id: tag.id)
 
     visit company_job_path(company, job)
-    save_and_open_page
 
     expect(page).to have_content "Tags: Developer"
+  end
+
+  scenario "they can see many tags" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    tag = Tag.create(name: "Developer")
+    tag2 = Tag.create(name: "Close to Home")
+    tag3 = Tag.create(name: "Daycare provided")
+    JobTag.create(job_id: job.id, tag_id: tag.id)
+    JobTag.create(job_id: job.id, tag_id: tag2.id)
+    JobTag.create(job_id: job.id, tag_id: tag3.id)
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content "Developer"
+    expect(page).to have_content "Close to Home"
+    expect(page).to have_content "Daycare provided"
   end
 end
